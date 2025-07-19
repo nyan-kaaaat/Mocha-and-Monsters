9776ff586b50a0f828dd8ce42001c46fb0618a73
Katherine Nguyen <katherinenguyen0205@gmail.com>
2025-07-19 15:40:23 -0600

    imported images of syrup bottles, created syrup buttons, a coffe maker 2d node and a button child node

--- mocha-and-monsters-game/controller/game.gd
+++ mocha-and-monsters-game/controller/game.gd
@@ -2,4 +2,12 @@ extends Node2D
 
 
 func _on_get_ingredient_pressed() -> void:
-	print("ingredients pressed")
+	print("flower pressed")
+
+
+func _on_syrup_button_bean_pressed() -> void:
+	print("bean pressed")
+
+
+func _on_syrup_button_cube_pressed() -> void:
+	print("cube pressed")
