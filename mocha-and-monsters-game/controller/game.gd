de6279f1364dbea62c32928841987733a86b4b33
Katherine Nguyen <katherinenguyen0205@gmail.com>
2025-07-19 16:05:00 -0600

    created buttons for coffee beans and a cup, imported sprites for them too

--- mocha-and-monsters-game/controller/game.gd
+++ mocha-and-monsters-game/controller/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 
+# the buttons on the ingredients shelf node
 func _on_get_ingredient_pressed() -> void:
 	print("flower pressed")
 
@@ -11,3 +12,13 @@ func _on_syrup_button_bean_pressed() -> void:
 
 func _on_syrup_button_cube_pressed() -> void:
 	print("cube pressed")
+
+
+func _on_coffee_beans_pressed() -> void:
+	print("coffee beans pressed")
+
+
+# coffe grinder buttons
+
+func _on_cup_size_pressed() -> void:
+	print("smallest cup pressed")
