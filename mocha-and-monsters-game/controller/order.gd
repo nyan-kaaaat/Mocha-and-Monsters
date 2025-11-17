b20c53e1857165351d26ff3c8e577682a356fda5
Katherine Nguyen <katherinenguyen0205@gmail.com>
2025-11-17 17:16:47 -0600

    added customer class and order scene, also added a 2d nodes for character

--- mocha-and-monsters-game/controller/order.gd
+++ mocha-and-monsters-game/controller/order.gd
@@ -1,11 +1,39 @@
 class_name Order extends Node2D
 
-var cup_size = null;
-var ingridients = []
+
+var cup_size
+var ingridients
+
+func _ready() -> void:
+	name.is_empty()
+	cup_size = null
+	ingridients = []
+
+func setName(string_name) -> void:
+	"""
+	Sets name of the order
+	
+	pre-conditions:
+		name: a string
+	"""
+	name = name
 
 func setCup(cup) -> void:
+	"""
+	sets desired cup size
+	
+	pre-conditions:
+		cup: cup object
+	"""
 	cup_size = cup
 	print("cup added")
 
 func addIngredient(ingreddient) -> void:
+	"""
+	adds ingredients to the ingredients list
+	
+	pre-conditions:
+		ingredient: ingredient objects
+	post-conditions: cup contains ingredients and updates it to screen
+	"""
 	ingridients.append(ingreddient)
